54e3df33b884be1f6655f665c04eb03d2865c3c1
Larry Jones <larryj@blinds.com>
2017-05-23 19:13:25 -0500

    Finally! The code works without warnings.

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -14,7 +14,7 @@
                    :email "metacirc@mit.edu" :classes [:6001 :6946]}
                   {:type :student :first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
                   {:type :student :first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
-                  {:type :professon :first "Harold" :last "Abelson" :email "evalapply@mit.edi"
+                  {:type :professor :first "Harold" :last "Abelson" :email "evalapply@mit.edi"
                    :classes [:6001]}]
          :classes {:6001 "The Structure and Interpretation of Computer Programs"
                    :6946 "The Structure and Interpretation of Classical Mechanics"
@@ -28,12 +28,48 @@
 (defn display-name [{:keys [first last] :as contact}]
   (str last ", " first (middle-name contact)))
 
+(defn student-view [student owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/li nil (display-name student)))))
+
+(defn professor-view [professor owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/li nil
+              (dom/div #js {:key (:first professor)} (display-name professor))
+              (dom/label nil "Classes")
+              (apply dom/ul nil
+                     (map #(dom/li nil %) (:classes professor)))))))
+
+(defmulti entry-view (fn [person _] (:type person)))
+
+(defmethod entry-view :student
+  [person owner] (student-view person owner))
+
+(defmethod entry-view :professor
+  [person owner] (professor-view person owner))
+
+(defn people [data]
+  (->> data
+       :people
+       (mapv (fn [p] 
+               (if (:classes p)
+                 (update-in p 
+                            [:classes]
+                            (fn [cs]
+                              (mapv (:classes data) cs)))
+                 p)))))
+
 (defn registry-view [data owner]
   (reify
-    om/IRenderState
-    (render-state [this state]
-      (dom/div nil
-               (dom/h2 nil "Contact list")))))
+    om/IRender
+    (render [_]
+      (dom/div #js {:id "registry"}
+               (dom/h2 nil "Registry")
+               (dom/div nil (om/build-all entry-view (people data)))))))
 
 (om/root registry-view
          app-state
